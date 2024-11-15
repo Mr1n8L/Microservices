5acd44598c1bff21b08adc21c1fd3f982a9cb469
Mr1n8L <mrinaljani1822@gmail.com>
2024-11-15 23:42:21 +0530

    service config

--- src/main/java/com/engagehub/api/service/CustomerService.java
+++ src/main/java/com/engagehub/api/service/CustomerService.java
@@ -4,7 +4,6 @@ import com.engagehub.api.model.Customer;
 import com.engagehub.api.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
